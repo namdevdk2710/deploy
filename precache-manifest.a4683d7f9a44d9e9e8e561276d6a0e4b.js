9d5a87a442f5d916628bb2cb101cb4e30cd3fa9f
Nam Dev <namdevdk2710@gmail.com>
2019-07-28 12:35:24 +0700

    Deploy

---
+++ precache-manifest.a4683d7f9a44d9e9e8e561276d6a0e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f073e2ff4360025ff4b735fd8e53c145",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f24d876bfdfdde6b8f5d",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b618eb2bcf413ad98a26",
+    "url": "/static/css/main.7ef435e9.chunk.css"
+  },
+  {
+    "revision": "f24d876bfdfdde6b8f5d",
+    "url": "/static/js/2.2b3cbfee.chunk.js"
+  },
+  {
+    "revision": "b618eb2bcf413ad98a26",
+    "url": "/static/js/main.6d5122fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
